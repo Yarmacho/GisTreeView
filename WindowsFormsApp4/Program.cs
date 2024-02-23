775e98e9e64919258f5fac3631ea240c0ca6507c
Yarmak Dmytro <dima.yrmak@gmail.com>
2024-02-23 18:41:05 +0200

    fix: ensure created database

--- WindowsFormsApp4/Program.cs
+++ WindowsFormsApp4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Database.DI;
 using DynamicForms.DependencyInjection;
 using DynamicForms.Factories;
+using Interfaces.Database.Abstractions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,8 +24,8 @@ namespace WindowsFormsApp4
             var host = CreateHostBuilder().Build();
             ServiceProvider = host.Services;
             FormFactory.ServiceProvider = ServiceProvider;
-            //ServiceProvider.GetRequiredService<IDbManager>()
-            //    .ReCreateAsync().GetAwaiter().GetResult();
+            ServiceProvider.GetRequiredService<IDbManager>()
+                .CreateAsync().GetAwaiter().GetResult();
             //ServiceProvider.GetRequiredService<IShapesManager>()
             //    .DeleteAllShapes(Configuration.GetValue<string>("MapsPath"));
 
